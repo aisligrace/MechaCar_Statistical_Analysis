6b330ee48d0a66b6c8f57d7607ad7fb680f20b91
aisligrace <96492230+aisligrace@users.noreply.github.com>
2022-04-16 12:44:06 -0400

    Add files via upload
    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -24,3 +24,14 @@ lot_summary <- mecha_suspension_coil  %>% group_by(Manufacturing_Lot) %>% summar
                                                                          Var_PSI=var(PSI),
                                                                          Std_Dev_PSI=sd(PSI),
                                                                          Num_Coil=n(), .groups = 'keep')  
+# Deliverable 3
+
+t.test(mecha_coil$PSI,mu=1500)
+
+lot1 <- subset(mecha_coil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(mecha_coil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(mecha_coil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
